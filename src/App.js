bcda03ee8b7e1db854e1c5fea96a1c6b62431c7f
goto920 <goto@kmgoto.jp>
2022-03-01 17:47:56 +0900

    guide moved

--- src/App.js
+++ src/App.js
@@ -528,7 +528,7 @@ https://stackoverflow.com/questions/52263471/how-to-create-a-mediastream-from-a-
     <hr />
     <div>
     Version: {packageJSON.version} &emsp; 
-    <a href="guide/" target="_blank" rel="noreferrer">Brief Guide</a>
+    <a href="./guide/" target="_blank" rel="noreferrer">Brief Guide</a>
     <a href="https://goto920.github.io/karaoke-video-recorder/" target="_blank" 
      rel="noreferrer"><br/>
     Manual/Update</a>&nbsp;on goto920.github.io<hr/>
